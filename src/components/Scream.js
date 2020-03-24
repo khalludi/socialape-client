a1e6a6fad2b2ea045ff8e44caa23f924889d6e95
Khalid Ali <khalludi123@gmail.com>
2020-03-23 23:48:33 -0400

    Start adding like and dislike buttons

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -3,12 +3,21 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime'
+import PropTypes from 'prop-types';
+import MyButton from '../util/MyButton';
 
 // MUI Stuff
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
+
+// Icons
+import ChatIcon from '@material-ui/icons/Chat';
+
+// Redux
+import { connect } from 'react-redux';
+import { likeScream, unlikeScream } from '../redux/actions/dataActions';
 
 const styles = {
     card: {
@@ -38,10 +47,33 @@ export class Scream extends Component {
                     <Typography variant="h5" component={Link} to={`/users/${userHandle}`} color="primary">{userHandle}</Typography>
                     <Typography variant="body2" color="textSecondary">{dayjs(createdAt).fromNow()}</Typography>
                     <Typography variant="body1">{body}</Typography>
+                    {likeButton}
+                    <span>{likeCount} Likes</span>
+                    <MyButton tip="Comments">
+                        <ChatIcon color="primary"/>
+                    </MyButton>
+                    <span>{commentCount} comments</span>
                 </CardContent>
             </Card>
         )
     }
 }
 
-export default withStyles(styles)(Scream);
+Scream.propTypes = {
+    likeScream: PropTypes.func.isRequired,
+    unlikeScream: PropTypes.func.isRequired,
+    user: PropTypes.object.isRequired,
+    scream: PropTypes.object.isRequired,
+    classes: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+    user: state.user
+})
+
+const mapActionsToProps = {
+    likeScream,
+    unlikeScream
+}
+
+export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(Scream));
